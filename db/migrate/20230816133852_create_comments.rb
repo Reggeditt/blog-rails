d06cf8609c3fb1c535be06c0638890229d686db7
Reggeditt <tkodjosarso@gmail.com>
2023-08-16 14:12:54 +0000

    add associations

--- db/migrate/20230816133852_create_comments.rb
+++ db/migrate/20230816133852_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, column: :author_id
       t.references :post, null: false, foreign_key: true
       t.text :text
 
