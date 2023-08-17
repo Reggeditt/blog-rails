80b662ed10a087357dcf517f173b1c5ef21a9a97
Reggeditt <tkodjosarso@gmail.com>
2023-08-17 00:01:04 +0000

    change attributes from :user to :author

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :user, foreign_key: 'author_id'
+  # belongs_to :user, foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  # alias_attribute :author, :user
 end
