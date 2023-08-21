ff51fb25fcc2d5209c8f8d2b2642d3d5456c0966
Reggeditt <tkodjosarso@gmail.com>
2023-08-21 12:52:05 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :posts
+  resources :users, only: [:index, :show] do
+  end
+
+  resources :posts, only: [:index, :show] do
+  end
+
 
   root 'users#index'
 end
