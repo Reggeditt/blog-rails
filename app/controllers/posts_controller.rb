3fb8bc2d805807704e3b59905ccf309618741e92
Reggeditt <tkodjosarso@gmail.com>
2023-08-25 13:23:05 +0000

    configure posts_controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @user = User.find_by(id: params[:user_id])
+    @user_posts = @posts.where(author_id: params[:user_id])
   end
 
   def show
-    @post = 'post'
+    @post = Post.find_by(id: params[:id]) || 'No posts yet'
+    @post_comments = Comment.all.where(post_id: params[:id])
+    @user = User.find_by(id: @post.author_id)
   end
 end
