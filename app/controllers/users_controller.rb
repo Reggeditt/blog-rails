c45669736092d5b002833ba195299a935770a6c7
Reggeditt <tkodjosarso@gmail.com>
2023-08-25 13:44:09 +0000

    fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,7 @@ class UsersController < ApplicationController
     @posts = Post.all || 'No posts yet'
     @post = []
     @posts.each do |post|
-      if post.author_id == @user.id
-        @post << post
-      end
+      @post << post if post.author_id == @user.id
     end
   end
 end
