e190975ec419c8dd374e9b517d80b0d8f251ff59
Reggeditt <tkodjosarso@gmail.com>
2023-08-31 23:44:01 +0000

    Create forms that Creates a Post for current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,5 +9,27 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id]) || 'No posts yet'
     @post_comments = Comment.all.where(post_id: params[:id])
     @user = User.find_by(id: @post.author_id)
+    @post_comment = @post.comments.build(author_id: @user.id)
+  end
+
+  def new
+    @user = current_user
+    @post = @user.posts.build
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.build(post_params)
+    if @post.save
+      @user.posts_counter += 1
+      @user.save
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
