15e6256a7d15352a6ecf8bf225e5a79d1c75ddba
Reggeditt <tkodjosarso@gmail.com>
2023-08-31 23:37:25 +0000

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts do
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create, :destroy]
+    end 
   end
 
   root 'users#index'
