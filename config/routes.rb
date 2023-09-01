ae42824a8ec4f24ad048e68c417fb273e1dce30e
Reggeditt <tkodjosarso@gmail.com>
2023-09-01 09:51:45 +0000

    update routes: eliminate multiple nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
+  concern :commentable do
+    resources :comments, only: [:create, :new]
+  end
+
+  concern :likeable do
+    resources :likes, only: [:create, :destroy]
+  end
+
   resources :users, only: [:index, :show] do
-    resources :posts do
-      resources :comments, only: [:create, :new]
-      resources :likes, only: [:create, :destroy]
-    end 
+    resources :posts, concerns: [:commentable, :likeable]
   end
 
   root 'users#index'
