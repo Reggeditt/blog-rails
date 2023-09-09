95f98c7fd56143845d25db483a32fc99b378d9a9
Reggeditt <tkodjosarso@gmail.com>
2023-09-09 12:11:16 +0000

    update views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_parameters, if: :devise_controller?, only: [:create]
+  before_action :authenticate_user!
+end
+
+
+def configure_devise_parameters
+  devise_parameter_sanitizer.permit(:sign_up) do |user|
+    user.permit(:name, :email, :password, :password_confirmation)
+  end
 end
